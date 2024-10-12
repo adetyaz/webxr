9e4389aa0767a10bc57d3984bf1d8fdfd44befe9
Suraj Mahto <surajmahto255@gmail.com>
2024-10-13 00:42:59 +0530

    feat : updated provider and wagmi

--- src/lib/wagmi.ts
+++ src/lib/wagmi.ts
@@ -1,35 +1,37 @@
-"use client";
 import { defaultWagmiConfig } from '@web3modal/wagmi/react/config'
-
-import { cookieStorage, createStorage } from 'wagmi'
+import { createStorage, cookieStorage } from 'wagmi'
 import { base } from 'wagmi/chains'
 
-// Get projectId from https://cloud.walletconnect.com
-export const projectId = 'c26b357532f63f7ba31efebac88d0eed'
+// Get projectId from environment variables
+export const projectId = process.env.NEXT_PUBLIC_PROJECT_ID
 
-if (!projectId) throw new Error('Project ID is not defined')
+if (!projectId) {
+  throw new Error('Project ID is not defined')
+}
 
-  export const metadata = {
-    name: 'AppKit',
-    description: 'AppKit Example',
-    url: 'https://web3modal.com', // origin must match your domain & subdomain
-    icons: ['https://avatars.githubusercontent.com/u/37784886']
-  }
+export const metadata = {
+  name: 'Myriadflow',
+  description: 'Myriadflow',
+  url: 'https://webxr.myriadflow.com', // Origin must match your domain & subdomain
+  icons: ['https://avatars.githubusercontent.com/u/37784886']
+}
 
-// Create wagmiConfig
+// Define supported chains
 const chains = [base] as const
+
+// Create Wagmi configuration
 export const config = defaultWagmiConfig({
   chains,
   projectId,
   metadata,
   auth: {
     email: true,
-    socials: ["github", "google", "x", "discord", "apple"],
-    showWallets: true, // default to true
+    socials: ['github', 'google', 'x', 'discord', 'apple'],
+    showWallets: true, // Defaults to true
     walletFeatures: true
   },
   ssr: true,
   storage: createStorage({
     storage: cookieStorage
   })
-});
+})
